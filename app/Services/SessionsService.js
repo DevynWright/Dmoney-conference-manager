fe367fa7e892c7c1eebcb67eabf21556286e9a09
DevynWright <Wrightdevyn@gmail.com>
2019-11-21 17:33:37 -0700

    It's Alive!!!!

--- app/Services/SessionsService.js
+++ app/Services/SessionsService.js
@@ -1,21 +1,34 @@
-import _store from "../store.js"
+import _store from "../store.js";
 import Speaker from "../Models/Speaker.js";
+import Session from "../Models/Session.js";
 
 class SessionsService {
   addSpeaker(speakerData) {
-    let speaker = new Speaker(speakerData)
-    let foundSession = _store.State.sessions.find(session => session.id == speaker.sessionId)
-    foundSession.speakers.push(speaker)
-    _store.saveState()
+    let speaker = new Speaker(speakerData);
+    let foundSession = _store.State.sessions.find(
+      session => session.id == speaker.sessionId
+    );
+    foundSession.speakers.push(speaker);
+    _store.saveState();
   }
-  deleteSpeaker (sessionId, speakerId) {
-    let foundSession = _store.State.sessions.find(session => session.id == sessionId)
-    foundSession.speakers = foundSession.speakers.filter(speaker=> speaker.id != speakerId)
-    _store.saveState()
+
+  addSession(sessionData) {
+    let freshSession = new Session(sessionData);
+    _store.State.sessions.push(freshSession)
   }
- constructor() {
-  console.log('Hello from sesionsService')
- }
+
+  deleteSpeaker(sessionId, speakerId) {
+    let foundSession = _store.State.sessions.find(
+      session => session.id == sessionId
+    );
+    foundSession.speakers = foundSession.speakers.filter(
+      speaker => speaker.id != speakerId
+    );
+    _store.saveState();
+  }
+  constructor() {
+    console.log("Hello from sesionsService");
+  }
 }
 
 const SESSIONSERVICE = new SessionsService();
